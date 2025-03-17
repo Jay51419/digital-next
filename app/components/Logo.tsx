7bf334ad09cf0da6e914fdf10e318383f7fabc2a
jay51419 <jaygandhi51419@gmail.com>
2025-03-17 16:19:16 +0530

    Image to img

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 export default function Logo() {
   return (
     <div className="flex w-full justify-center items-center gap-2">
-      <Image className="w-36 h-36" src="/A3B-new-logo.png" alt="Logo" />
+      <img className="w-36 h-36" src="/A3B-new-logo.png" alt="Logo" />
     </div>
   );
 }
