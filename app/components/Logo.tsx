a7cb3b7acd0dc9e9bd0ab74cf204b8199e371c7f
jay51419 <jaygandhi51419@gmail.com>
2025-03-17 16:22:53 +0530

    logo ratio

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <div className="flex w-full justify-center items-center gap-2">
-      <img className="w-36 h-36" src="/A3B-new-logo.png" alt="Logo" />
+      <img className="w-36" src="/A3B-new-logo.png" alt="Logo" />
     </div>
   );
 }
